39f1ceab18eb995283cffbeb614224eb290d5d69
Kamali Selvaraj <kamali.selvaraj@aspiresys.com>
2021-07-05 18:13:56 +0530

    Python

---
+++ 01-July-2021/required_argument_1.py
@@ -0,0 +1,15 @@
+print("Program for required arguments")
+print('Example 1')
+def fun(a, b):
+    print("Value of a and b before swapping")
+    print(a, b)
+    a, b = b, a
+    print("Value of a and b after swapping")
+    print(a, b)
+fun()
+print('**************************************')
+ 
+
+
+
+    
